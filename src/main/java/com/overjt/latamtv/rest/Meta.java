a3761d3278c9227f788039c8de56e1aeabb08e7c
Jonathan Toledo <kry0ster@gmail.com>
2021-02-16 21:57:48 -0500

    Stremio Endpoints

---
+++ src/main/java/com/overjt/latamtv/rest/Meta.java
@@ -0,0 +1,39 @@
+package com.overjt.latamtv.rest;
+
+
+public class Meta {
+    private String id;
+    private String type;
+    private String name;
+    private String poster;
+
+    public void setID(String id){
+        this.id = id;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPoster(String poster){
+        this.poster = poster;
+    }
+
+    public String getID(){
+        return this.id;
+    }
+    public String getType(){
+        return this.type;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPoster(){
+        return this.poster;
+    }
+
+    
+}
